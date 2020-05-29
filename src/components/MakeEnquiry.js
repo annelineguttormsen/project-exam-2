351049daa1e12ae753ad792f98b154af9b2d2989
Anneline <anneline.guttormsen@hotmail.no>
2020-05-29 02:38:14 +0200

    oppdatering makeenquiry
    fjernet yup + react hook form, laget egen onsubmit funksjon som sjekker form input values

--- src/components/MakeEnquiry.js
+++ src/components/MakeEnquiry.js
@@ -7,17 +7,8 @@ import {
     useParams,
     useHistory
 } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
 
 import ErrorMessage from "./ErrorMessage";
-
-const schema = yup.object().shape({
-	clientName: yup.string().required("Name is required"),
-    email: yup.string().email("E-mail must be valid").required("E-mail is required"),
-    // fromDate: yup.number().required("Check-in date is required"),
-    // toDate: yup.number("Must be a number").required("Check-out date is required")
-});
 
 export default function MakeEnquiry() {
     let { id } = useParams();
@@ -27,12 +18,12 @@ export default function MakeEnquiry() {
         ca: []
     });
 
+    const [formState,setFormState] = useState({
+        formSubmitted: false
+    });
+
     //globale variabler
     let hotelId, fromDate, toDate;
-
-    const { register, handleSubmit, errors } = useForm({
-        validationSchema: schema
-    });
 
     try {
         hotelId = id.match(/id=(.*)&fromDate/)[1];
@@ -47,7 +38,6 @@ export default function MakeEnquiry() {
             fetch("/establishments.json")
             .then(response => response.json())
             .then(responseJSON => {
-                console.log("response json er", responseJSON);
                 let currentArticle = responseJSON.find(i => i.id === hotelId);
                 setState({ca: currentArticle});
             })
@@ -56,12 +46,47 @@ export default function MakeEnquiry() {
             });
         }, []
     );
+    
+    function onSubmit(event) {
+        console.log(event.target[3].value);
 
-    function onSubmit(data, event) {
-        //TODO: fjern preventdefault
-        event.preventDefault();
-        console.log(data);
+        let emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+        let dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+        //test om  er tomme
+        if (event.target[0].value === undefined || event.target[0].value === "") {
+            console.log("0");
+            event.preventDefault();
+            return false
+        }
+        //sjekk om date input er en dato
+        if (!dateRegex.test(event.target[1].value)) {
+            console.log("1");
+            event.preventDefault();
+            return false
+        }
+        if (!dateRegex.test(event.target[1].value)) {
+            console.log("2");
+            event.preventDefault();
+            return false
+        }
+        //sjekk om navn er tomt
+        if (event.target[3].value === undefined || event.target[3].value === "") {
+            console.log("3");
+            event.preventDefault();
+            return false
+        }
+        //sjekk om input er valid e-post
+        if (!emailRegex.test(event.target[4].value)) {
+            event.preventDefault();
+            console.log("4");
+            return false
+        }
+        setTimeout(function() {
+            setFormState({formSubmitted: true});
+        },100);
+        console.log("onsubmit kalt!");
     }
+    console.log("public url", process.env.PUBLIC_URL);
 
     return (
         <div className="col-6 col-md-8 col-sm-12 makeenquiry container">
@@ -71,12 +96,19 @@ export default function MakeEnquiry() {
                     <li className="breadcrumbs__link"><Link to={"/makeenquiry/" + id}>Make enquiry</Link></li>
                 </ul>
             </div>
+            {formState.formSubmitted ? 
+            <>
+            <h1>Thank you for your booking!</h1>
+            <p>We have sent you an e-mail with more details about your booking, if you have any more questions please head over to <Link to="/contact">our contact us page</Link>.</p>
+            </>
+             : 
+            <>
             <h1>Your booking</h1>
             <form
                 method="POST" 
-                action="enquiry-success.php"
+                action={process.env.PUBLIC_URL + "enquiry-success.php"}
                 target="hiddenframe"
-                // onSubmit={handleSubmit(onSubmit)}
+                onSubmit={onSubmit}
             >
                 <label>Establishment
                 <input
@@ -84,41 +116,41 @@ export default function MakeEnquiry() {
                     name="establishment"
                     type="text"
                     value={state.ca["establishmentName"]}
+                    required
                 /></label>
-                <label>Check-in <span>* </span>{errors.fromDate && <ErrorMessage text={errors.fromDate.message}/>}
+                <label>Check-in <span>* </span>
+                {/* {errors.fromDate && <ErrorMessage text={errors.fromDate.message}/>} */}
                 <input
                     className="col-12 makeenquiry__input" 
                     name="checkin"
                     type="date"
                     defaultValue={fromDate}
-                    // ref={register({required:true})}
+                    required
                 /></label>
-                <label>Check-out <span>* </span>{errors.toDate && <ErrorMessage text={errors.toDate.message}/>}
+                <label>Check-out <span>* </span>
                 <input
                     className="col-12 makeenquiry__input" 
                     name="checkout"
                     type="date"
                     defaultValue={toDate}
-                    // ref={register({required:true})}
+                    required
                 /></label>
                 <h2>Contact information</h2>
-                <label>Full name <span>* </span>{errors.clientName && <ErrorMessage text={errors.clientName.message}/>}
+                <label>Full name <span>* </span>
                 <input 
                     className="col-12 makeenquiry__input" 
                     name="clientName"
                     type="text" 
                     placeholder="John Smith"
-                    ref={register({required:true})}
-                    // onChange={(event) => updateInput(event, "search")}
+                    required
                 /></label>
-                <label>E-mail <span>* </span>{errors.email && <ErrorMessage text={errors.email.message}/>}
+                <label>E-mail <span>* </span>
                 <input 
                     className="col-12 makeenquiry__input"  
                     name="email"
                     type="text" 
                     placeholder="jonathan.smithsonian@gmail.com"
-                    ref={register({required:true})}
-                    // onChange={(event) => updateInput(event, "search")}
+                    // required
                 /></label>
                 <button 
                     className="btn btn--success"
@@ -127,8 +159,11 @@ export default function MakeEnquiry() {
                     <p>Make booking</p>
                 </button>
             </form>
+            </>
+            }
+            {/*slutt på conditional operator (?)*/}
             {/* https://stackoverflow.com/a/30666118 */}
-            <iframe name="hiddenframe" className="hidden__frame" title="Hidden frame"/>
+            <iframe name="hiddenframe"  title="Hidden frame"/>
         </div>
     )
 }
